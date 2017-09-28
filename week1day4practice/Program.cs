897364b079bae8a7885ad476d4c44e1b0d41682b
mahajanl <lakshamima@gmail.com>
2017-09-28 15:11:47 -0400

    primary commit

---
+++ week1day4practice/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1day4practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string firstName = "Lakshami";
+            //Console.WriteLine("You have " + firstName.Length + " in your name.");
+
+            //string lastName = "Mahajan";
+            //Console.WriteLine("You have " + lastName.Length + " in your last name.");
+
+            Console.WriteLine("Type in your first name for me.");
+            string lastName = Console.ReadLine();
+            int lastNameLeng = lastName.Length;
+
+            Console.WriteLine("Type in your last name for me,");
+            string firstName = Console.ReadLine();
+            int firstNAmeLeng = lastName.Length;
+
+            if(lastNameLeng > firstNAmeLeng)
+            {Console.WriteLine("Your last name is longer than your first name.");
+            }
+            else if(firstName.Length == lastName.Length)
+            { Console.WriteLine("They're the same!");
+            }
+            else
+            { Console.WriteLine("Last name is longer!");
+            }
+
+            Console.WriteLine("This is a \t tab" + "\nThis is a new line" + "\nThis is a \abell sound.");
+        }
+    }
+}
