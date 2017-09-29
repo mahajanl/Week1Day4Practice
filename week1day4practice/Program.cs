b2d811507e3aa4814f2bc27d3abf47aee6926c8d
mahajanl <lakshamima@gmail.com>
2017-09-28 20:34:26 -0400

    more practice with == and equals

--- week1day4practice/Program.cs
+++ week1day4practice/Program.cs
@@ -31,12 +31,36 @@ namespace week1day4practice
             //{ Console.WriteLine("They're the same!");
             //}
             //else
-            //{ Console.WriteLine("Last name is longer!");
+            //{ Console.WriteLine("Your last name is longer!");
             //}
 
             //Console.WriteLine("This is a \t tab" + "\nThis is a new line" + "\nThis is a \abell sound.");
 
-            string nameOne = "Harry";
+            //string nameOne = "Harry";
+            //string nameTwo = "Cleo";
+
+            //bool strEq = (nameOne == nameTwo);
+
+            //if (nameOne == nameTwo) 
+            //{ Console.WriteLine("These names are the same!");
+            //}
+
+            //else if (nameOne != nameTwo) 
+            //{ Console.WriteLine("These names are different. How cool is that?");
+            //}
+
+            // this is not part of what I want to do::
+            // Console.WriteLine(strEq);
+
+            //Console.WriteLine(nameOne.Equals(nameTwo));
+
+            Console.WriteLine("Hi there! Can I have your first name?");
+            string uneNom = Console.ReadLine();
+
+            Console.WriteLine("Can I now have your last name as well?");
+            string deuxNom = Console.ReadLine();
+
+            Console.WriteLine("Your name is " + uneNom + " " + deuxNom + ".");
 
         }
     }
